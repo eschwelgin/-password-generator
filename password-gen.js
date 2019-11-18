caad2d25efa916d78f4e22340a78a85804b61e9f
eschwelgin <eschwelgin@gmail.com>
2019-11-18 12:57:51 -0500

    finished logic for multiple char sets and fixed bugutput

--- password-gen.js
+++ password-gen.js
@@ -1,4 +1,4 @@
- alert("This app will generate a random password")
+// alert("This app will generate a random password")
 var finalPass1 = [] //create an array
 //creeate variables to store values while they convert from an array to a string
 var finalPass2 = ""
@@ -6,10 +6,10 @@ var finalPass3 = ""
 
 //basic - set a variable for the password length 
 //advanced - take input from input form <-------------------------------------------------------------------------------------INCOMPLETE----------------------------------------------------------
-var pLength = 29
+var pLength = 128
 // document.getElementById("pLengthBox").value
 // variables for char sets - called by user input checkboxes 
-var lcInclude = true
+var lcInclude = false
 var ucInclude = true
 var nmInclude = true
 var spInclude = true
@@ -19,14 +19,105 @@ var lcCharset = [ "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d
 //advanced - create 4 arrays - one for each charset and connect them to the input boxes for use 
 var ucCharset = [ "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M" ]
 var nmCharset = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" ]
-var spCharset = [ "!", "$", "%", "&", "(", ")", "+", "-", "/", "<", "?", "[", "]", "^", "_", "{", "|", "}", "~" ] //deleted items with special meaning to prevent errors 
+var spCharset = [ "!", "$", "%", "&", "(", ")", "+", "-", "/", "?", "[", "]", "^", "_", "{", "|", "}", "~" ] //deleted items with special meaning to prevent errors 
 
 // for loop that creates a password of the right length
 for (i = 0; i < pLength; i++) {
-//logic to create a password of length pLength from character set lcCharset 
-finalPass1[i] = (lcCharset[Math.floor(Math.random() * lcCharset.length)])
-//if [user input]=lowercase then include lcCharSet
-
+//basic - logic to create a password of length pLength from character set lcCharset 
+//finalPass1[i] = (lcCharset[Math.floor(Math.random() * lcCharset.length)])
+//advanced - include character sets based on values of variables
+    if (lcInclude === true && ucInclude === false && nmInclude === false && spInclude === false) { //<-- one box
+        finalPass1[i] = (lcCharset[Math.floor(Math.random() * lcCharset.length)])
+    } else if (lcInclude === false && ucInclude === true && nmInclude === false && spInclude === false) {
+        finalPass1[i] = (ucCharset[Math.floor(Math.random() * ucCharset.length)])
+    } else if (lcInclude === false && ucInclude === false && nmInclude === true && spInclude === false) {
+        finalPass1[i] = (nmCharset[Math.floor(Math.random() * nmCharset.length)])
+    } else if (lcInclude === false && ucInclude === false && nmInclude === false && spInclude === true) {
+        finalPass1[i] = (spCharset[Math.floor(Math.random() * spCharset.length)])
+    } else if (lcInclude === true && ucInclude === true && nmInclude === false && spInclude === false) { //<-- two box
+        if (Math.floor(Math.random() * 2) === 1) { 
+            finalPass1[i] = (lcCharset[Math.floor(Math.random() * lcCharset.length)]) 
+        } else { 
+            finalPass1[i] = (ucCharset[Math.floor(Math.random() * ucCharset.length)])
+            }
+    } else if (lcInclude === true && ucInclude === false && nmInclude === true && spInclude === false) { //<-- two box
+        if (Math.floor(Math.random() * 2) === 1) { 
+            finalPass1[i] = (lcCharset[Math.floor(Math.random() * lcCharset.length)]) 
+        } else { 
+            finalPass1[i] = (nmCharset[Math.floor(Math.random() * nmCharset.length)])
+            }
+    } else if (lcInclude === true && ucInclude === false && nmInclude === false && spInclude === true) { //<-- two box
+        if (Math.floor(Math.random() * 2) === 1) { 
+            finalPass1[i] = (lcCharset[Math.floor(Math.random() * lcCharset.length)]) 
+        } else { 
+            finalPass1[i] = (spCharset[Math.floor(Math.random() * spCharset.length)])
+            }
+    } else if (lcInclude === false && ucInclude === true && nmInclude === true && spInclude === false) { //<-- two box
+        if (Math.floor(Math.random() * 2) === 1) { 
+            finalPass1[i] = (ucCharset[Math.floor(Math.random() * ucCharset.length)]) 
+        } else { 
+            finalPass1[i] = (nmCharset[Math.floor(Math.random() * nmCharset.length)])
+            }
+    } else if (lcInclude === false && ucInclude === true && nmInclude === false && spInclude === true) { //<-- two box
+        if (Math.floor(Math.random() * 2) === 1) { 
+            finalPass1[i] = (ucCharset[Math.floor(Math.random() * ucCharset.length)]) 
+        } else { 
+            finalPass1[i] = (spCharset[Math.floor(Math.random() * spCharset.length)])
+            }
+    } else if (lcInclude === false && ucInclude === false && nmInclude === true && spInclude === true) { //<-- two box
+        if (Math.floor(Math.random() * 2) === 1) { 
+            finalPass1[i] = (nmCharset[Math.floor(Math.random() * nmCharset.length)]) 
+        } else { 
+            finalPass1[i] = (spCharset[Math.floor(Math.random() * spCharset.length)])
+            }
+    } else if (lcInclude === true && ucInclude === true && nmInclude === true && spInclude === false) { //<-- three box 123
+        var randomNum = (Math.floor(Math.random() * 3))
+        if (randomNum === 1) { 
+            finalPass1[i] = (lcCharset[Math.floor(Math.random() * lcCharset.length)]) 
+        } else if (randomNum === 2){ 
+            finalPass1[i] = (ucCharset[Math.floor(Math.random() * ucCharset.length)])
+        } else {
+            finalPass1[i] = (nmCharset[Math.floor(Math.random() * nmCharset.length)])
+        } 
+    } else if (lcInclude === true && ucInclude === false && nmInclude === true && spInclude === true) { //<-- three box 134
+        var randomNum = (Math.floor(Math.random() * 3))
+        if (randomNum === 1) { 
+            finalPass1[i] = (lcCharset[Math.floor(Math.random() * lcCharset.length)]) 
+        } else if (randomNum === 2){ 
+            finalPass1[i] = (nmCharset[Math.floor(Math.random() * nmCharset.length)])
+        } else {
+            finalPass1[i] = (spCharset[Math.floor(Math.random() * spCharset.length)])
+        } 
+    } else if (lcInclude === true && ucInclude === true && nmInclude === false && spInclude === true) { //<-- three box 124
+        var randomNum = (Math.floor(Math.random() * 3))
+        if (randomNum === 1) { 
+            finalPass1[i] = (lcCharset[Math.floor(Math.random() * lcCharset.length)]) 
+        } else if (randomNum === 2){ 
+            finalPass1[i] = (ucCharset[Math.floor(Math.random() * ucCharset.length)])
+        } else {
+            finalPass1[i] = (spCharset[Math.floor(Math.random() * spCharset.length)])
+        } 
+    } else if (lcInclude === false && ucInclude === true && nmInclude === true && spInclude === true) { //<-- three box 234
+        var randomNum = (Math.floor(Math.random() * 3))
+        if (randomNum === 1) { 
+            finalPass1[i] = (ucCharset[Math.floor(Math.random() * ucCharset.length)]) 
+        } else if (randomNum === 2){ 
+            finalPass1[i] = (nmCharset[Math.floor(Math.random() * nmCharset.length)])
+        } else {
+            finalPass1[i] = (spCharset[Math.floor(Math.random() * spCharset.length)])
+        } 
+    } else {                                                                                             //<-- four box
+        var randomNum = (Math.floor(Math.random() * 4))
+        if (randomNum === 1) { 
+            finalPass1[i] = (lcCharset[Math.floor(Math.random() * lcCharset.length)]) 
+        } else if (randomNum === 2) { 
+            finalPass1[i] = (ucCharset[Math.floor(Math.random() * ucCharset.length)])
+        } else if (randomNum === 3) { 
+            finalPass1[i] = (spCharset[Math.floor(Math.random() * spCharset.length)])
+        } else {
+            finalPass1[i] = (nmCharset[Math.floor(Math.random() * nmCharset.length)])
+        } 
+    } 
 } //end for loop 
 
 //convert password from an array to a string
@@ -38,21 +129,14 @@ finalPass3 = (finalPass2.replace(/,/g, ""))
 function myFunction () {
     document.getElementById("passOut").innerHTML = finalPass3;
     document.getElementById("passOut").style.display = "inline-block";
-    // validate user input <-- needs to be nested in the submit function somewhere 
-    if (pLength >= 8 && pLength <= 128) {
-        alert("Generating password - checkpoint 1")
-    } else {
+    // validate user input
+    if (pLength < 8 || pLength > 128) {
         alert("Please select a number in between 8 and 128")
-    }
-
+    } 
     if (lcInclude === false && ucInclude === false && nmInclude === false && spInclude === false) {
-        alert("Please select 1+ Character Sets")
-    } else {
-        alert("Generating password - checkpoint 2")
-    }
+        alert("Please select 1+ Character Sets" + "checkpoint 2 fail")
+    } 
 }
-// advanced - button press consideres user input and creates password, then displays it <--------------------------------INCOMPLETE--------------------------------------------------------------
-
 // bonus - copy button for password <------------------------------------------------------------------------------------INCOMPLETE--------------------------------------------------------------
 
 //-----------------------------------------------------
@@ -73,5 +157,5 @@ function myFunction () {
 // Watch the password convert from a string to a clean array
 // console.log(finalPass1)
 // console.log(finalPass2)
-// console.log(finalPass3)
-
+console.log(finalPass3)
+console.log(finalPass3.length)
